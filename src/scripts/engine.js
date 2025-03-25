26d6cbd6d0f4f83ce1bd98cd82a39044ddd237a0
@Geovanni_JDA <109145436+GeovanniJDA@users.noreply.github.com>
2025-03-25 17:45:25 -0300

    add cards

---
+++ src/scripts/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+  "💵",
+  "💵",
+  "😱",
+  "😱",
+  "😡",
+  "😡",
+  "🤡",
+  "🤡",
+  "👹",
+  "👹",
+  "😸",
+  "😸",
+  "👽",
+  "👽",
+  "🦊",
+  "🦊"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5) ? 2 : -1);
+
+for(i = 0; i < emojis.length; i++){
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  document.querySelector(".game").appendChild(box)
+}
