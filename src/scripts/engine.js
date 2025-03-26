d29313b61abcab78b83272a1cc4889e331b83f4d
@Geovanni_JDA <109145436+GeovanniJDA@users.noreply.github.com>
2025-03-26 16:43:08 -0300

    update features

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,20 +1,20 @@
 const emojis = [
-  "💵",
-  "💵",
-  "😱",
-  "😱",
-  "😡",
-  "😡",
-  "🤡",
-  "🤡",
-  "👹",
-  "👹",
-  "😸",
-  "😸",
-  "👽",
-  "👽",
   "🦊",
-  "🦊"
+  "💵",
+  "😡",
+  "😱",
+  "😡",
+  "💵",
+  "🤡",
+  "😱",
+  "👹",
+  "🤡",
+  "😸",
+  "👹",
+  "👽",
+  "😸",
+  "🦊",
+  "👽"
 ];
 let openCards = [];
 
@@ -24,5 +24,33 @@ for(i = 0; i < emojis.length; i++){
   let box = document.createElement("div");
   box.className = "item";
   box.innerHTML = shuffleEmojis[i];
+  box.onclick = handleClick;
   document.querySelector(".game").appendChild(box)
+}
+
+function handleClick(){
+  if(openCards.length < 2){
+    this.classList.add("boxOpen");
+    openCards.push(this)
+  }
+
+  if(openCards.length == 2){
+    setTimeout(checkMatch, 500)
+  }
+}
+
+function checkMatch(){
+  if(openCards[0].innerHTML === openCards[1].innerHTML){
+    openCards[0].classList.add("boxMatch");
+    openCards[1].classList.add("boxMatch");
+  }else{
+    openCards[0].classList.remove("boxOpen");
+    openCards[1].classList.remove("boxOpen");
+  }
+
+  openCards = [];
+
+  if(document.querySelectorAll(".boxMatch").length === emojis.length){
+    alert("Você venceu!")
+  }
 }
